3a371ac2a32e1d534427168fbc02e954a1374f60
Pritam Sinha <7629288+sinha-p@users.noreply.github.com>
2021-04-04 14:13:13 +0530

    Feat/new site (#1)
    * my new site
    
    * added build
    
    * updated in profile
    
    * removed build dir

---
+++ src/components/header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { ThemeToggler } from 'gatsby-plugin-dark-mode';
+
+
+import '../styles/header.scss';
+
+// components
+import Toggle from './toggle';
+import Github from './icons/github';
+import Linkedin from './icons/linkedin';
+import Codepen from './icons/codepen';
+
+
+
+const links = [
+    {
+        href: 'https://github.com/sinha-p',
+        label: 'github',
+    },
+    {
+        href: 'https://codepen.io/pri_io',
+        label: 'codepen',
+    },
+    {
+        href: 'https://www.linkedin.com/in/pritam-sinha-a5856b10b/',
+        label: 'linkedin',
+    }
+]
+
+const Header = () => {
+    const renderIcon = (iconName) => {
+        switch (iconName) {
+            case 'github': return <Github />;
+            case 'codepen': return <Codepen />;
+            case 'linkedin': return <Linkedin />;
+            default: return null;
+        }
+    }
+    return (
+    <nav className="nav">
+        <div className="container nav-wrapper flex v-align space-btw">
+            <ul className="links">
+                {
+                    links.map(({ href, label }) => (
+                        <li key={label} className="list">
+                            <a href={href} className="social-icon">
+                                {renderIcon(label)}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
+            <ThemeToggler>
+                {({ theme, toggleTheme }) => (
+                    <Toggle theme={theme} toggleTheme={toggleTheme} />
+                )}
+            </ThemeToggler>
+        </div>
+    </nav>
+)};
+
+export default Header;
